432f5cabf07253cd9e374bfb543a8a43b58c91f9
Swain Molster <swain.molster@gmail.com>
2019-08-02 13:04:27 -0400

    Updating project. Swift 5, better documentation.

--- DateCacher/DateCacher.swift
+++ DateCacher/DateCacher.swift
@@ -3,7 +3,7 @@
 //  DateCacher
 //
 //  Created by Swain Molster on 7/3/18.
-//  Copyright © 2018 Swain Molster. All rights reserved.
+//  Copyright © 2019 Swain Molster. All rights reserved.
 //
 
 import Foundation
@@ -15,47 +15,105 @@ public protocol DateFormat {
 
 open class DateCacher {
     
-    public typealias FormatterCache = [String: DateFormatter]
-    public typealias ResultCache = [String: (stringCache: [Date: String], dateCache: [String: Date])]
+    internal typealias FormatterCache = [String: DateFormatter]
+    internal typealias ResultCache = [String: (stringCache: [Date: String], dateCache: [String: Date])]
     
-    /// Keys are date format strings.
+    /**
+     Cache holding already-configured `DateFormatter` objects.
+     
+     Keys are date format strings. Values are `DateFormatter` objects configured for the format key.
+     */
     internal private(set) var formatterCache: FormatterCache = [:]
+    
+    /**
+     Cache holding already-constructed `String`/`Date` pairs, organized according to their format strings.
+     
+     Keys are date format strings. Values are a pair of mirrored `String`/`Date` caches, one keyed by `String`s, the other by `Date`s, for easy access using either object.
+     
+     _Example Usage_
+     ```
+     resultCache["MM-dd-yyyy"]?.dateCache["01-01-2001"] // -> Date?
+     ```
+     */
     internal private(set) var resultCache: ResultCache = [:]
     
-    private let calendar: Calendar?
+    private let calendar: Calendar
     
-    public init(calendar: Calendar? = .current) {
+    /**
+     Creates a `DateCacher` using the provided `Calendar`. Default is `Calendar.current`.
+     */
+    public init(calendar: Calendar = .current) {
         self.calendar = calendar
     }
     
+    /**
+     Returns a `Date` object created from the given `string` and `format`, or `nil` if the `string` is not formatted correctly.
+     
+     - Parameters:
+       - string: The date string. Should be formatted according to the provided `DateFormat`.
+       - format: The expected format of the provided date `string`.
+     
+     This function relies on result caching to optimize it's efficiency.
+     */
     open func date(from string: String, using format: DateFormat) -> Date? {
+        // Check for a cached result keyed by the provided `string`. If available, return it.
         if let cachedResult = resultCache[format.formatString]?.dateCache[string] {
             return cachedResult
         }
 
+        // Grab a `DateFormatter` for the provided `format`, and use it to make a `Date`.
         guard let date = formatter(for: format).date(from: string) else {
+            // If this goes wrong, it's likely the `string`/`format` pair is not matched correctly. We return `nil`.
             return nil
         }
         
+        // Try to simplify the `Date`. See explanation below in `string(date:format:)`.
         let dateToReturn = simplifiedDate(for: date, using: format) ?? date
+        
+        // Store the simplified date.
         cache((string, dateToReturn), for: format)
         
         return dateToReturn
     }
     
+    /**
+     Returns a `String` object created from the given `date` and `format`.
+     
+     - Parameters:
+       - date: The Date object.
+       - format: The expected format of the provided date `string`.
+     
+     This function relies on result caching to optimize it's efficiency.
+     */
     open func string(from date: Date, using format: DateFormat) -> String {
         
+        // Try to simplify the `date`. We don't want to store any components that aren't relevant
+        // to the provided `DateFormat`. Storing non-relevant components could lead to redundant entries
+        // in the result cache.
+        //
+        // For example, our `DateFormat` string may be "MM-dd-yyyy", but the provided `Date` may contain
+        // information regarding hours or minutes. We want to strip this information from the `Date` object,
+        // so as to only include a single entry for a given "MM-dd-yyyy".
+        //
         let date = simplifiedDate(for: date, using: format) ?? date
         
+        // Check for a cached result keyed by the provided (and simplified) `date`. If available, return it.
         if let cachedResult = resultCache[format.formatString]?.stringCache[date] {
             return cachedResult
         }
         
+        // Otherwise, create a new formatted `String` and cache it.
         let string = formatter(for: format).string(from: date)
         cache((string, date), for: format)
         return string
     }
     
+    /**
+     Returns a `DateFormatter` object configured for the provided `format`, pulling from the formatter cache if possible.
+     
+     - parameter format: The desired configuration format.
+     
+     */
     internal func formatter(for format: DateFormat) -> DateFormatter {
         if formatterCache[format.formatString] == nil {
             formatterCache[format.formatString] = DateFormatter(formatString: format.formatString)
@@ -63,12 +121,30 @@ open class DateCacher {
         return formatterCache[format.formatString]!
     }
     
+    /**
+     Returns a simplified version of the provided `date`, or `nil` if a simplified `Date` could not be constructed. Specifically, returns a new `Date` constructed _only_ from the `DateComponents` relevant to the provided `format`.
+     
+     - Parameters:
+       - date: The `Date` to simplify.
+       - format: The desired `DateFormat`.
+     */
     internal func simplifiedDate(for date: Date, using format: DateFormat) -> Date? {
-        guard let calendar = calendar else { return nil }
-        let dateComponents = calendar.dateComponents(format.components, from: date)
-        return calendar.date(from: dateComponents)
+        let dateComponents = self.calendar.dateComponents(format.components, from: date)
+        
+        if let newDate = self.calendar.date(from: dateComponents) {
+            return newDate
+        } else {
+            return nil
+        }
     }
     
+    /**
+     Stores the provided string/date `pair` into the cache.
+     
+     - Parameters:
+       - pair: The `String`/`Date` pair to store.
+       - format: The `DateFormat` of the provided `String`/`Date` pair.
+     */
     internal func cache(_ pair: (string: String, date: Date), for format: DateFormat) {
         if resultCache[format.formatString] == nil {
             resultCache[format.formatString] = ([:], [:])
